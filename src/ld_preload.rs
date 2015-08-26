e4ec4c052993553a5c44d3d686ae6e8fb4b824de
Geoffrey Thomas <geofft@ldpreload.com>
2015-08-26 19:28:07 -0400

    Move LD_PRELOAD implementation to separate module

---
+++ src/ld_preload.rs
@@ -0,0 +1,69 @@
+use libc::{c_char, c_void};
+
+#[link(name="dl")]
+extern {
+        fn dlsym(handle: *const c_void, symbol: *const c_char) -> *const c_void;
+}
+
+const RTLD_NEXT: *const c_void = -1isize as *const c_void;
+
+pub unsafe fn dlsym_next(symbol: &'static str) -> *const u8 {
+    let ptr = dlsym(RTLD_NEXT, symbol.as_ptr() as *const c_char);
+    if ptr.is_null() {
+        panic!("redhook: Unable to find underlying function for {}", symbol);
+    }
+    ptr as *const u8
+}
+
+/* Rust doesn't directly expose __attribute__((constructor)), but this
+ * is how GNU implements it. */
+#[link_section=".init_array"]
+pub static INITIALIZE_CTOR: extern fn() = ::initialize;
+
+#[macro_export]
+macro_rules! hook {
+    (fn $real_fn:ident ( $($v:ident : $t:ty),* ) -> $r:ty => $hook_fn:ident $body:block) => {
+        #[allow(non_camel_case_types)]
+        pub struct $real_fn {__private_field: ()}
+        #[allow(non_upper_case_globals)]
+        static $real_fn: $real_fn = $real_fn {__private_field: ()};
+
+        impl $real_fn {
+            fn get(&self) -> unsafe extern fn ( $($v : $t),* ) -> $r {
+                use ::std::sync::{Once, ONCE_INIT};
+
+                static mut REAL: *const u8 = 0 as *const u8;
+                static mut ONCE: Once = ONCE_INIT;
+
+                unsafe {
+                    ONCE.call_once(|| {
+                        REAL = $crate::ld_preload::dlsym_next(concat!(stringify!($real_fn), "\0"));
+                    });
+                    ::std::mem::transmute(REAL)
+                }
+            }
+
+            #[no_mangle]
+            pub unsafe fn $real_fn ( $($v : $t),* ) -> $r {
+                if $crate::initialized() {
+                    $hook_fn ( $($v),* )
+                } else {
+                    $real_fn.get() ( $($v),* )
+                }
+            }
+        }
+
+        pub fn $hook_fn ( $($v : $t),* ) -> $r {
+            $body
+        }
+    };
+
+    (fn $real_fn:ident ( $($v:ident : $t:ty),* ) => $hook_fn:ident $body:block) => {
+        hook! { fn $real_fn ( $($v : $t),* ) -> () => $hook_fn $body }
+    };
+}
+
+#[macro_export]
+macro_rules! real {
+    ($real_fn:ident) => {$real_fn.get()}
+}
