006ae993e1f70cd25be51ccf432dbd623ac6c3ca
Geoffrey Thomas <geofft@ldpreload.com>
2015-09-05 01:19:25 -0400

    Add a super simple test runner

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+preload () {
+    local library
+    library=$1
+    shift
+    if [ "$(uname)" = "Darwin" ]; then
+        DYLD_INSERT_LIBRARIES=target/debug/"$library".dylib "$@"
+    else
+        LD_PRELOAD=target/debug/"$library".so "$@"
+    fi
+}
+
+set -ex
+
+cd examples/readlinkspy
+cargo build
+preload libreadlinkspy ls -l /dev/stdin | grep readlink
